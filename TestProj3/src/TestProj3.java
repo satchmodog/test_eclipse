eefdf07c22f67ed190c209b420941fff4fb4da49
satchmodog <texas.brichards@gmail.com>
2016-08-16 08:14:15 -0500

    Delete .gitattributes

---
+++ TestProj3/src/TestProj3.java
@@ -0,0 +1,12 @@
+
+public class TestProj3 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is TestProj3.");
+		TestProj2 newTest2 = new TestProj2();
+		
+		System.out.println(newTest2.getNewPrintString());
+	}
+
+}
